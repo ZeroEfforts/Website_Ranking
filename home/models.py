fca6c445f12a51f9270d431245835b13f9d30df9
GHOST HUNT <50262751+ZeroEfforts@users.noreply.github.com>
2020-08-31 13:29:38 +0500

    search history

--- home/models.py
+++ home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class SearchHistory(models.Model):
+
+    keyword = models.CharField(max_length=255, null=False)
+    wesite = models.CharField(null=False)
+    pause = models.IntegerField(null=False, default=2)
+
+    class Meta:
+        verbose_name = "SearchHistory"
+        verbose_name_plural = "SearchHistorys"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("SearchHistory_detail", kwargs={"pk": self.pk})
