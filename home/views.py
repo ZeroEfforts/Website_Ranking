a1e131bf4394206bb07a5927e80ff42110acff6e
GHOST HUNT <50262751+ZeroEfforts@users.noreply.github.com>
2020-08-31 14:27:26 +0500

    forms

--- home/views.py
+++ home/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .forms import SearchForm
 # Create your views here.
+
+def index(request):
+    form=SearchForm()
+    if request=="POST":
+        pass
+
+    context={
+        'form':form
+    }
+    return render(request, 'home/index.html', context)
