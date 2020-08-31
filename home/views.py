1e0df8f67a899acefa04ed0cacbee688886cdcdf
GHOST HUNT <50262751+ZeroEfforts@users.noreply.github.com>
2020-08-31 14:47:29 +0500

    gsearch

--- home/views.py
+++ home/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
 from .forms import SearchForm
+from ..private.gsearch import gsearch
 # Create your views here.
 
 def index(request):
     form=SearchForm()
-    if request=="POST":
-        pass
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            data=form.save()
+            searchresult(data)
+            return redirect('home:result data.id')
 
     context={
         'form':form
     }
     return render(request, 'home/index.html', context)
+
+def result(request, pk):
+    pass
+
+def searchresult(request,data):
+    count, weblist=gsearch(query=data.keyword, pause=data.pause)
+    context={
+        'total':count,
+        'websites':weblist
+    }
+    return render(request, 'home/result.html', context)
