a46b4bd0757afd1334d4c6f238ead7e38b5d05b3
GHOST HUNT <50262751+ZeroEfforts@users.noreply.github.com>
2020-08-31 22:41:31 +0500

    results working

--- home/views.py
+++ home/views.py
@@ -12,8 +12,8 @@ def index(request):
         form = SearchForm(request.POST)
         if form.is_valid():
             data = form.save()
-            result, weblist = searchresult(request, data)
-            return redirect('home:result', pk=result.id, websites=weblist)
+            result= searchresult(request, data)
+            return redirect('home:result', pk=result.id)
 
     context = {
         'form': form
@@ -21,8 +21,10 @@ def index(request):
     return render(request, 'home/index.html', context)
 
 
-def result(request, pk, websites):
+def result(request, pk):
     result = SearchResult.objects.get(id=pk)
+    with open(".temp", "r") as fp:
+        websites = json.load(fp)
     context = {
         'total': result.totalwebsites,
         'websites': websites
@@ -33,14 +35,15 @@ def result(request, pk, websites):
 
 def searchresult(request, data):
     results = gsearch(query_=data.keyword, pause_=data.pause)
+    with open(".temp", "w") as fp:
+        json.dump(results,fp)
     try:
-        return(
-            SearchResult.objects.create(
-                searchhistory=data,
-                totalwebsites=len(results),
-                websiterank=1
-            ),
-            results
+        return SearchResult.objects.create(
+            searchhistory=data,
+            totalwebsites=len(results),
+            websiterank=1
         )
+
     except Exception as err:
         return err
+
